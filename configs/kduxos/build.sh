32e761cba71b8331f115cf92b2ff5662453639ae
Spaceboy Ross <ross@bluelightos.ddns.net>
2019-03-19 08:59:09 -0700

    Started configuring

--- configs/kduxos/build.sh
+++ configs/kduxos/build.sh
@@ -2,10 +2,10 @@
 
 set -e -u
 
-iso_name=archlinux
-iso_label="ARCH_$(date +%Y%m)"
-iso_publisher="Arch Linux <http://www.archlinux.org>"
-iso_application="Arch Linux Live/Rescue CD"
+iso_name=kduxos
+iso_label="KDUX_$(date +%Y%m)"
+iso_publisher="KduxOS <https://github.com/kduxos>"
+iso_application="KduxOS Live/Rescue CD"
 iso_version=$(date +%Y.%m.%d)
 install_dir=arch
 work_dir=work
