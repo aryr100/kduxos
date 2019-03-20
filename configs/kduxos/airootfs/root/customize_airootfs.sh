1a061cc6b3640073750cedad16bec4e8e6c29cfb
Spaceboy Ross <ross@bluelightos.ddns.net>
2019-03-20 11:28:13 -0700

    Fixed gpg errors

--- configs/kduxos/airootfs/root/customize_airootfs.sh
+++ configs/kduxos/airootfs/root/customize_airootfs.sh
@@ -22,4 +22,5 @@ sed -i 's/#\(HandleLidSwitch=\)suspend/\1ignore/' /etc/systemd/logind.conf
 systemctl enable pacman-init.service choose-mirror.service NetworkManager.service
 systemctl set-default multi-user.target
 
+gpg --recv-keys 1DCE2C057903C34D
 pacman-key --init
